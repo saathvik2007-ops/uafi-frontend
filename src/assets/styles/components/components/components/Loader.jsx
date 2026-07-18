eb7e8e64744bca1c8a70a263acd4265b0dcee8df
saathvik2007-ops <saathvik2007@gmail.com>
2026-07-18 20:13:37 +0530

    Create Loader.jsx

---
+++ src/assets/styles/components/components/components/Loader.jsx
@@ -0,0 +1,22 @@
+function Loader() {
+
+    return (
+
+        <div style={{
+            display:"flex",
+            justifyContent:"center",
+            alignItems:"center",
+            padding:"40px",
+            fontSize:"20px",
+            color:"#1A73E8"
+        }}>
+
+            Loading...
+
+        </div>
+
+    );
+
+}
+
+export default Loader;
